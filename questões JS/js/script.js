22ace45c861b4302df69715e0ebea5f3c4e59f31
DaviBavaresco <daviantoniobavaresco@gmail.com>
2022-09-02 20:44:39 -0300

    finalizado

---
+++ questões JS/js/script.js
@@ -0,0 +1,20 @@
+
+
+
+function media(){
+    
+    while(nome!=0){
+        
+
+    var nome = prompt('Qual seu nome');
+    var n1 = Number(prompt('primeira nota'));
+    var n2 = Number(prompt('segunda nota'));
+    var soma = (n1 + n2)/2
+    alert("aluno " + nome + " sua média é de: "+ soma)    
+    }
+
+    
+    
+}
+media()
+
